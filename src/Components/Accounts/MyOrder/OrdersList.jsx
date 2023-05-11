b8ec7ad02152d70c33ff3e63f79f405509fa5e42
Haritscode <haritsharma058@gmail.com>
2023-05-11 16:53:45 +0530

    11/05/2023 orderDetails fixed

--- src/Components/Accounts/MyOrder/OrdersList.jsx
+++ src/Components/Accounts/MyOrder/OrdersList.jsx
@@ -7,7 +7,6 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 export default function OrdersList() {
     const userId=useSelector(state=>state.rootReducer.userData.userId)
-    const [sortedData,setSortedData]=useState([]);
     const {id}=useParams();
     const [data=[],loading,error]=ordersList(id,userId.slice(3)||localStorage.getItem("uid"));
     const [orderList,setOrderList]=useState([]);
@@ -18,13 +17,12 @@ export default function OrdersList() {
         let item=[...data];
         item?.sort((a,b)=>a.orderTimeStampId<b.orderTimeStampId?1:-1)
         setOrderList(item);
-        console.log(item);
       }
     },[data])
     return (
     <>
     <ol className='orderList'>
-    {!loading?orderList.map((item,count)=><button onClick={()=>navigate(`/${id}/order`,{state:{orderId:item.orderId}})}><OrderDetails key={count} item={item}/></button>):""}
+    {!loading?orderList.map((item,count)=><button key={count} onClick={()=>navigate(`/${id}/order`,{state:{orderId:item.orderId}})}><OrderDetails item={item}/></button>):""}
     </ol>
     </>
   )
